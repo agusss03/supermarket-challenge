2e789e4c078579c7ff124e9234202a5639c9b710
agusss03 <agus22dominguez@gmail.com>
2023-07-07 13:43:09 -0300

    Finished working on the supermarket challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { SupermarketItem, SupermarketItems } from "./types/itemTypes";
 
@@ -10,31 +10,46 @@ function App() {
     description: "",
     price: 0,
   });
-  
-  
-  const [itemList, setItemList] = useState<SupermarketItems>([]);
+  const [itemList, setItemList] = useState<SupermarketItems>(() => JSON.parse(localStorage.getItem("itemList") ?? "[]"));
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("itemList", JSON.stringify(itemList));
+  }, [itemList]);
+
   const handleAddItem = () => {
     const id = (Math.random() * 1000).toString() + newItem.name;
-    setItemList([...itemList, { ...newItem, id }]);
     setOpenModal(false);
+    setIsLoading(true)
+    setItemList([...itemList, { ...newItem, id }]);
     setNewItem({ id: "", name: "", description: "", price: 0 });
+
+    setTimeout(() => {
+      setItemList([...itemList, { ...newItem, id }]);
+      setIsLoading(false);
+    }, 1500);
+
   };
-  
+
   const handleItemChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name: dynamicNameValue, value } = e.target;
     setNewItem({ ...newItem, [dynamicNameValue]: value });
   };
-  
-  const handleItemRemove = (id: string) => {
-    setItemList(oldValues => {
-      return oldValues.filter(itemList => itemList.id !== id)
-    })
-  };
 
-  return (
+  const handleItemRemove = (id: string) => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setItemList((oldValues) => oldValues.filter((itemList) => itemList.id !== id));
+      setIsLoading(false);
+    }, 1500);
+  };
+  
+  const isAddButtonDisabled = newItem.name === ""
+
+   return (
     <main className="bg-[#bccdd7] w-screen h-screen relative flex justify-center items-center">
       {openModal ? (
-        <div className="bg-[#e0efee] rounded-lg p-3 justify-center content-center w-[20%] mx-auto h-[25%]">
+        <div className="bg-[#e0efee] rounded-lg p-5 justify-center content-center w-auto mx-auto h-auto">
           <div className="flex flex-col items-center">
             <h1 className="mb-5 text-[20px] font-semibold text-[#1c2020]">
               Add items
@@ -45,7 +60,7 @@ function App() {
               placeholder=" Name"
               onChange={handleItemChange}
               value={newItem.name}
-              className="mb-4 rounded-lg h-8 w-full"
+              className="mb-4 rounded-lg h-8 w-full ps-1"
             />
             <input
               name="description"
@@ -53,7 +68,7 @@ function App() {
               placeholder=" Description"
               onChange={handleItemChange}
               value={newItem.description}
-              className="mb-4 rounded-lg h-8 w-full"
+              className="mb-4 rounded-lg h-8 w-full ps-1"
             />
             <input
               name="price"
@@ -61,7 +76,7 @@ function App() {
               placeholder="Price"
               onChange={handleItemChange}
               value={newItem.price}
-              className="mb-4 rounded-lg h-8 w-full"
+              className="mb-4 rounded-lg h-8 w-full ps-2"
             />
           </div>
           <div className="flex items-center gap-4 w-full justify-center">
@@ -74,34 +89,48 @@ function App() {
             <button
               className="bg-[#38a284] w-40 rounded-lg mt-5 p-0.5 text-[#dcf7f9] font-medium"
               onClick={handleAddItem}
+              disabled={isAddButtonDisabled}
             >
               Add
             </button>
           </div>
         </div>
       ) : (
-        <div className="bg-[#e0efee] rounded-lg p-4 justify-center content-center w-[16%] mx-auto h-auto">
+        <div className="bg-[#e0efee] rounded-lg p-5 justify-center content-center w-auto mx-auto h-auto min-w-[18%]">
           <div className="flex flex-col items-center">
             <h1 className="mb-5 text-[20px] font-semibold text-[#1c2020]">
               Supermarket List
             </h1>
-            <div className="text-[#1c2020] text-[18x]">{itemList.length} Items added</div>
-            <div className="mt-2 mb-2 text-[#1c2020] ">
-              {itemList.map((item) => (
-                <div className="flex gap-10 m-3">
-                  <button className="rounded-full bg-[#bb4168] w-5 h-5 flex items-center justify-center pb-1 text-[#dcf7f9]" onClick={() => handleItemRemove(item.id)}>
-                    x
-                  </button>
-                  <p>{item.description}</p>
+            {isLoading ? (
+                <div className="flex justify-center items-center">
+                <div className="flex w-full items-center justify-center gap-2">
+                  <div className="flex h-6 w-6 items-center justify-center rounded-full bg-gradient-to-tr from-[#a9d3d3] to-[#47cea8] animate-spin">
+                    <div className="h-4 w-4 rounded-full bg-gray-200"></div>
+                  </div>
+                  <text>Loading...</text>
                 </div>
-              ))}
+              </div>
+              ) : (
+            <div className="flex flex-col items-center content-center">
+              <div className="text-[#1c2020] text-[18x]">{itemList.length} Items added</div>
+                <div className="mt-2 mb-2 text-[#1c2020] ">
+                  {itemList.map((item) => (
+                    <div className="flex gap-10 m-3">
+                      <button className="rounded-full bg-[#bb4168] w-5 h-5 flex items-center justify-center pb-1 text-[#dcf7f9]" onClick={() => handleItemRemove(item.id)}>
+                        x
+                      </button>
+                      <p>{item.description}</p>
+                    </div>
+                      ))}
+                </div>
+                  <button
+                    onClick={() => setOpenModal(true)}
+                    className="bg-[#296b6e] w-40 rounded-lg mt-5 p-0.5 text-[#dcf7f9] font-medium"
+                  >
+                    Add
+                  </button>
             </div>
-            <button
-              onClick={() => setOpenModal(true)}
-              className="bg-[#296b6e] w-40 rounded-lg mt-5 p-0.5 text-[#dcf7f9] font-medium"
-            >
-              Add
-            </button>
+            )}
           </div>
         </div>
       )}
